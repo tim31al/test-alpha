4c6b59902649df9c6cf16561ff49353962ce97d0
Alexandr Timofeev <tim31al@gmail.com>
2022-05-31 11:35:59 +0300

    init script fix

--- init.sh
+++ init.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+# init.sh
 
+set -e
+
+docker-compose up -d db
+
+echo "Build frontend"
 docker-compose build frontend
 
 docker-compose up -d
+echo "Wakeup services"
+sleep 10
 
+echo "Install dependencies"
 docker-compose exec app composer install
+
+echo "Migrate"
 docker-compose exec -u app app php bin/console doctrine:migrations:migrate -q
+
+echo "Load fixtures"
 docker-compose exec -u app app php bin/console doctrine:fixtures:load --group AppFixtures -q
